7cea27ad0030b4c44ba73783cfdea6d6124c6000
darius <dariusz.skrzypek87@gmail.com>
2021-04-01 04:40:33 +0200

    reverted logic in endpoint to use server data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,20 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {ApiService} from './services/api.service';
-import {Subject} from 'rxjs';
-import {takeUntil} from 'rxjs/operators';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent {
     venueList: any[] = ['Lokal 1', 'Lokal 2', 'Lokal 3'];
     venueValue: string;
     dateFrom: any;
     dateTo: any;
     reportCollection: any;
     retrievingData = false;
-    unSubscribe = new Subject();
 
     constructor(private apiService: ApiService) {
-    }
-    ngOnInit() {
-        this.apiService.fakeServer$.pipe(takeUntil(this.unSubscribe)).subscribe((collection) => this.updateCollection(collection));
-    }
-
-    ngOnDestroy() {
-        this.unSubscribe.next();
-        this.unSubscribe.complete();
     }
 
     retrieveReport() {
@@ -35,12 +24,11 @@ export class AppComponent implements OnInit, OnDestroy {
 
                 const params = {
                     venue: this.venueValue,
-                    dateFrom: this.dateFrom,
-                    dateTo: this.dateTo
+                    dateFrom: new Date(this.dateFrom).getTime(),
+                    dateTo: new Date(this.dateTo).getTime(),
                 };
 
-                //this.apiService.retrieveExportData(params).subscribe((collection) => this.updateCollection(collection));
-                this.apiService.retrieveExportData(params);
+                this.apiService.retrieveExportData(params).subscribe((collection) => this.updateCollection(collection));
             } else {
                 console.log('Processing request...');
             }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,71 +1,16 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {Subject} from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
 })
 export class ApiService {
 
-    fakeServer$ = new Subject();
-    fakeResponse = [
-        {
-            name: 'Test',
-            date: '2018-02-26',
-            time: '12:00',
-            user: 'User1',
-            venue: 'Lokal 1'
-        },
-        {
-            name: 'Test',
-            date: '2018-02-11',
-            time: '12:00',
-            user: 'User1',
-            venue: 'Lokal 1'
-        },
-        {
-            name: 'Test',
-            date: '2018-02-23',
-            time: '12:00',
-            user: 'User1',
-            venue: 'Lokal 1'
-        },
-        {
-            name: 'Test',
-            date: '2018-02-21',
-            time: '8:00',
-            user: 'User2',
-            venue: 'Lokal 1'
-        },
-        {
-            name: 'Test',
-            date: '2018-02-11',
-            time: '12:00',
-            user: 'User4',
-            venue: 'Lokal 3'
-        },
-        {
-            name: 'Test',
-            date: '2018-02-23',
-            time: '12:00',
-            user: 'User1',
-            venue: 'Lokal 2'
-        },
-        {
-            name: 'Test',
-            date: '2018-05-23',
-            time: '16:00',
-            user: 'User3',
-            venue: 'Lokal 1'
-        },
-    ];
-
     constructor(private http: HttpClient) {
     }
 
     retrieveExportData(params) {
-        // return this.http.get('localhost:8000/desired/url/', params);
-        this.fakeServer$.next(this.fakeResponse);
+        return this.http.get('localhost:8000/exports/retrieve', params);
     }
 }
 
